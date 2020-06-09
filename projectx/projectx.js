3460180fc05a6474ce2e51c00e0015b8ef048881
thefulger <63638262+thefulger@users.noreply.github.com>
2020-06-09 17:19:50 +0600

    projectx

---
+++ projectx/projectx.js
@@ -0,0 +1,90 @@
+//alert("Hello, world");
+
+// jai
+var message; // любой текст для того что работать 
+
+message = "Hello, Nursat";
+var number;
+number = 10;
+var bool;
+bool = true;
+var mUndefined = undefined;
+var mNull = null;
+var myobject = {
+    name: "Nursat", 
+    age: 18
+};
+
+//  number
+/*var res = 40 + 10;
+
+console.log(40 + 10);
+console.log(40 + 10);
+console.log(40 + 10);
+console.log(40 + 10);
+console.loh(Math.round(5.15));
+console.log(Math.ceil(.561));
+console.log(Math.floor(.561));
+console.log(res.toFixed(1));
+*/
+
+
+/*
+//string
+console.log("40" + number);
+console.log(message.toLowerCase());
+*/
+
+//array
+/*
+var names = ["nurs", "fulger"];
+console.log(names[0].toUpperCase());
+
+names.push("Hey");
+console.log(names);
+
+
+*/
+
+//if
+/*
+if(101 > 5){
+   console.log(true);
+}*/
+
+
+//cicl
+/*
+for(var i = 0;i < 10;i++){
+    console.log(i);
+}
+for(var i = 0;i < names.length; i++){
+    console.log(names[i])
+}
+*/
+
+//function
+function test(x, y){
+    console.log(x + y);
+}
+
+test(1, 23);
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
